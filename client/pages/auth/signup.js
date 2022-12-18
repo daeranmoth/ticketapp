3997af1641a5f32d58b4f448ba4b4a0264925d77
gary camboulives <gary.camboulives@gmail.com>
2022-12-18 18:21:44 +0100

    signup page ok

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 const signup = () => {
@@ -12,12 +12,13 @@ const signup = () => {
       email,
       password,
     },
+    onSuccess: () => Router.push("/"),
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    doRequest();
+    await doRequest();
   };
 
   return (
