79e4ca8a3d57a370a420ad43ac0ca3cd203104f7
gary camboulives <gary.camboulives@gmail.com>
2022-12-19 10:58:04 +0100

    frontend auth flow ok

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,26 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+//because next.js doesn t handle multiple getInitialProps
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return { pageProps, currentUser: data.currentUser };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,11 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing page</h1>;
+  return currentUser ? (
+    <h1>you are signed in</h1>
+  ) : (
+    <h1>you are NOT signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
