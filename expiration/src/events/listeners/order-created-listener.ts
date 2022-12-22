410cedea9de3016949210bc87b48c43934e415e6
gary camboulives <gary.camboulives@gmail.com>
2022-12-23 00:05:59 +0100

    expiration service with redis setup ok

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,15 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@gcticketapp/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
